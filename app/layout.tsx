8a3c74d49f68ebe54b307e1b781f6b3cddb88909
Alex Straight <compsciaj@outlook.com>
2024-11-01 00:26:19 -0700

    Added theme provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import localFont from 'next/font/local';
 import './globals.css';
 import { Toaster } from 'sonner';
+import { ThemeProvider } from '@/providers/theme-provider';
 
 const geistSans = localFont({
   src: './fonts/GeistVF.woff',
@@ -29,8 +30,15 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <main>{children}</main>
-        <Toaster />
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <main>{children}</main>
+          <Toaster />
+        </ThemeProvider>
       </body>
     </html>
   );
