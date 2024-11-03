3f867583f6d8945d47bafd92b83e2e75986aae09
Alex Straight <compsciaj@outlook.com>
2024-11-02 22:48:35 -0700

    Ignore test files in prod build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      // Exclude test files from server build
+      config.module.rules.push({
+        test: /\.test\.(js|ts|tsx)$/,
+        use: 'ignore-loader',
+      });
+    }
+    return config;
+  },
+  /* other config options here */
 };
 
 export default nextConfig;
